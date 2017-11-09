d6bd4dd11e0dfcb2496a14f7c1dab1c6f7029d97
Harmen J. Bussemaker <hjb2004@columbia.edu>
2017-11-08 21:08:56 -0500

    added package-level man page stub

---
+++ R-package/NRLB/R/package-NRLB.R
@@ -0,0 +1,11 @@
+#' NRLB: utitilies for working with NRLB models
+#' 
+#' The NRLB package provides tools for working with protein-DNA recognition models created by the No Read Left Behind (NRLB) algorithm developed by the Bussemaker Lab in the Department of Biological Sciences at Columbia University.
+#'
+#' 
+#' @section Foo functions:
+#' The foo functions ...
+#'
+#' @docType package
+#' @name NRLB
+NULL
