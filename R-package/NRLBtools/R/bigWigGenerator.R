2d522a9038348943a2f29f0ce30a606ff873f502
Harmen Bussemaker <hjb2004@columbia.edu>
2018-01-02 21:25:01 -0500

    renamed function for making bigwig files according to Chaitanya's method (i.e., sum both strands for each window and then to each base pair assign the maximum over all windows containing it)

--- R-package/NRLBtools/R/bigWigGenerator.R
+++ R-package/NRLBtools/R/bigWigGenerator.R
@@ -1,11 +1,15 @@
-#' Read in binary files containing genomic scoring results
+#' Create bigwig file from raw genomic profile
 #' 
 #' @param profiles Raw genomic score profiles, created using \code{fetch.genomic.profiles}
 #' @param footprint Footprint size of the NRLB model that was used to generate the genomic profile.
 #' @param file Name of the bigwig file that is to be created.
 #' @param truncate.tol Cutoff below which affinities are assumed to be zero.
+#' @param normalize Whether to use genomic maximum ('genome') or optimal site for model ('model') as reference for relative binding affinity. Note that model needs to be specified when the latter option is used.
+#' @param model Model used for normalization based on theoretical optimal site (see 'optimize').
 #'
 #' @return None
+#' 
+#' @note First, the affinity scores for the forward and reverse strand are added up for each window. Next, the value assigned to any particular base pair is the maximum over all windows containing that base pair.
 #' 
 #' @examples
 #' 
@@ -15,16 +19,21 @@
 #'                             model = m, 
 #'                             chr.names = c("chrXHet", "chrYHet"))
 #' 
-#' create.bigwig(profiles = gp, file = tempfile(fileext=".bw"), model = m)
+#' my.bwfile = tempfile(fileext=".bw")
+#' create.bigwig.chaitanya(profiles = gp, file = my.bwfile, model = m)
+#' 
+#' bw = import(my.bwfile)
+#' bw
 #'
 #' @export
 #' 
-create.bigwig = function(profiles, file, model, truncate.tol = 1E-4, normalize = "model") {
+create.bigwig.chaitanya = function(profiles, file, model=NULL, truncate.tol = 1E-4, normalize = "genome") {
 
   if (normalize == "genome") {
     reference.score = NRLBtools::genomic.max(profiles)
     cat("normalizing scores by genomic maximum\n")
   } else if (normalize == "model") {
+    if (is.null) error("model needs to be defined")
     reference.score = NRLBtools::optimal.site(model$fits, model$index, model$mode)$score
   } else {
     error("normalize argument needs to be 'model' or 'genome'")
