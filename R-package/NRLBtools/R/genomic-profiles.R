27cc71716a1b01827dc6802aa897edc4b7e03d99
Harmen Bussemaker <hjb2004@columbia.edu>
2018-01-05 16:57:11 -0500

    added functionality for generating genomic tracks in GRanges format and examples of how to generate BigWig files; also expanded the set of supported fly models to all available ExdHox complexes and Hox monomers

--- R-package/NRLBtools/R/genomic-profiles.R
+++ R-package/NRLBtools/R/genomic-profiles.R
@@ -1,4 +1,82 @@
-#' Fetch pre-computed genomic score profiles
+#' Fetch pre-computed genomic score profiles as GRanges object
+#' 
+#' @param url.prefix Prefix of URL containing precomputed profiles
+#' @param genome Complete genome sequence in BSgenome format
+#' @param chr.names Chromosome(s) to which profile should be restricted (optional; the default is to include profiles for all chromosomes in the genome)
+#' @param model An NRLB model (required to determine footprint)
+#' @param rc Whether to swap forward and reverse profiles
+#'
+#' @return A nested list containing forward and reverse NRLB score profiles for each chromosome.
+#' 
+#' @examples
+#' 
+#' m = NRLBtools::hox.models()$ExdScr
+#' gr = fetch.as.track("http://bussemakerlab.org/NRLB/dm3/ExdScr", 
+#'                     genome = BSgenome.Dmelanogaster.UCSC.dm3, 
+#'                     model = m, normalize = "none",
+#'                     chr.names = c("chrXHet", "chrYHet")
+#'                     )
+#' gr
+#' 
+#' v = Views(BSgenome.Dmelanogaster.UCSC.dm3$chrXHet, start=1:6, width=footprint.size(m))
+#' sapply(v, function(x) score.genome(x, model=m))
+#'
+#' @export
+#'
+fetch.as.track = function(url.prefix, genome, chr.names = NULL, normalize = "none", model=NULL) {
+ 
+  if (is.null(chr.names)) { chr.names = seqnames(genome) }
+  
+  fs = NRLBtools::footprint.size(model)
+  
+  gr.list = lapply(chr.names, function(chr) {
+    
+    cat(sprintf("fetching %s [%s] ...", url.prefix, chr), "\n")
+    cl = seqlengths(genome)[chr]
+    
+    url = sprintf("%s/_%s_F.dat", url.prefix, chr)
+    score.fwd = readBin(url, what = "double", endian = "big", n = cl-fs+1)
+    url = sprintf("%s/_%s_R.dat", url.prefix, chr)
+    score.rev = readBin(url, what = "double", endian = "big", n = cl-fs+1)
+    #url = sprintf("%s/_%s_Flag.dat", url.prefix, chr)
+    #flag = readBin(url, what = "double", endian = "big", n = cl-fs+1)
+
+    if (model$rc == TRUE) { fwd.strand = "-"; rev.strand = "+" } else {fwd.strand = "+"; rev.strand = "-"}
+    
+    gr.fwd = GRanges(seqnames = rep(chr, length(score.fwd)), 
+                     ranges = IRanges(start=1:length(score.fwd), width=fs),
+                     strand = fwd.strand,
+                     score = score.fwd)
+    gr.rev = GRanges(seqnames = rep(chr, length(score.rev)), 
+                     ranges = IRanges(start=1:length(score.rev), width=fs),
+                     strand = rev.strand,
+                     score = score.rev)
+    
+    c(gr.fwd, gr.rev)
+  })
+  gr = suppressWarnings(do.call("c", gr.list)) # suppress warning about non-overlapping seqnames
+  seqinfo(gr) = seqinfo(genome)[chr.names]
+  
+  if (normalize == "genome") {
+    reference.score = max(gr$score)
+    cat("normalizing scores by genomic maximum\n")
+    gr$score = gr$score / reference.score
+  } else if (normalize == "model") {
+    if (is.null(model)) error("model needs to be defined")
+    reference.score = NRLBtools::optimal.site(model$fits, model$index, model$mode)$score
+    cat("normalizing scores by theoretical maximum\n")
+    gr$score = gr$score / reference.score
+  } else if (normalize == "none") {
+    cat("using raw scores (not recommended)\n")
+  } else {
+    error("normalize argument needs to be 'model' or 'genome'")
+  }
+  
+  return(gr)
+}
+
+
+#' Fetch pre-computed genomic score profiles as nested list object
 #' 
 #' @param genome Complete genome sequence in BSgenome format
 #' @param url.prefix Prefix of URL containing precomputed profiles
--- R-package/NRLBtools/R/hox-models.R
+++ R-package/NRLBtools/R/hox-models.R
@@ -15,5 +15,8 @@ hox.models = function() {
   df$file = system.file("extdata/fitsets", df$file, package = "NRLBtools")
   df$fits = lapply(df$file, file.parser)
   nested.list = apply(df, 1, as.list)
+  
+  for(i in which(is.na(sapply(nested.list, function(x) x$mode)))) nested.list[[i]]$mode = NULL # turn NA into NULL
+
   return(nested.list)
 }
